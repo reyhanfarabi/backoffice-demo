01ab067254f75151dbf428bf2d0db15b05567030
mreyhanfarabi@ubuntuwsl2 <reyhanfarabione@outlook.com>
2024-04-30 22:31:52 +0700

    add base table component

---
+++ src/components/table/BaseTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="overflow-x-auto text-sm">
+    <table class="w-full">
+      <thead class="bg-neutral-200 dark:bg-neutral-700">
+        <tr>
+          <th
+            scope="col"
+            class="p-3 first:rounded-l last:rounded-r text-left"
+            v-for="header in props.headers"
+            :key="header"
+          >
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          class="border-b last:border-0 border-neutral-300 dark:border-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800"
+          v-for="(data, index) in props.datalist"
+          :key="index"
+        >
+          <td scope="row" class="p-3" v-for="item in data" :key="item">{{ item }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface IBaseTableProps {
+  headers: string[]
+  datalist: (string | number)[][]
+}
+
+const props = defineProps<IBaseTableProps>()
+</script>
