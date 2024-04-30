acdcfac0ffce17bf3fb0a83244396a43097c02e0
mreyhanfarabi@ubuntuwsl2 <reyhanfarabione@outlook.com>
2024-04-30 22:36:25 +0700

    add changes to routes
    remove temporary route for testing
    add types to routes
    add categories page route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,19 @@
+import ListPage from '@/views/Categories/ListPage.vue'
 import MainView from '@/views/MainView.vue'
-import TestPage1 from '@/views/TestPage1.vue'
-import TestPage1Child1 from '@/views/TestPage1Child1.vue'
-import TestPage2 from '@/views/TestPage2.vue'
+import type { RouteComponent } from 'vue-router'
 
-export const routes = [
+export interface IRouteBase {
+  path: string
+  name: string
+  component: RouteComponent
+}
+
+export interface IRoute extends IRouteBase {
+  icon: string
+  children: IRouteBase[]
+}
+
+export const routes: IRoute[] = [
   {
     path: '/',
     name: 'Dashboard',
@@ -11,29 +21,36 @@ export const routes = [
     icon: 'pi-home',
     children: []
   },
+  // {
+  //   path: '/page1',
+  //   name: 'Page 1',
+  //   icon: 'pi-clipboard',
+  //   component: TestPage1,
+  //   children: [
+  //     {
+  //       path: '/',
+  //       name: 'Page 1',
+  //       component: TestPage1
+  //     },
+  //     {
+  //       path: '/child1',
+  //       name: 'Child 1',
+  //       component: TestPage1Child1
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/page2',
+  //   name: 'Page 2',
+  //   component: TestPage2,
+  //   icon: 'pi-file-plus',
+  //   children: []
+  // },
   {
-    path: '/page1',
-    name: 'Page 1',
-    icon: 'pi-clipboard',
-    component: TestPage1,
-    children: [
-      {
-        path: '/',
-        name: 'Page 1',
-        component: TestPage1
-      },
-      {
-        path: '/child1',
-        name: 'Child 1',
-        component: TestPage1Child1
-      }
-    ]
-  },
-  {
-    path: '/page2',
-    name: 'Page 2',
-    component: TestPage2,
-    icon: 'pi-file-plus',
+    path: '/categories',
+    name: 'Categories',
+    component: ListPage,
+    icon: 'pi-th-large',
     children: []
   }
 ]
