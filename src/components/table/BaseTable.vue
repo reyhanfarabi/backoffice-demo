6b981e512477421e0e9d6a4a639fa60ae0cc7bbe
mreyhanfarabi@ubuntuwsl2 <reyhanfarabione@outlook.com>
2024-05-02 00:40:32 +0700

    add config to table component
    add width config to specify cell width

--- src/components/table/BaseTable.vue
+++ src/components/table/BaseTable.vue
@@ -3,12 +3,8 @@
     <table class="w-full">
       <thead class="bg-neutral-200 dark:bg-neutral-700">
         <tr>
-          <th
-            scope="col"
-            class="p-3 first:rounded-l last:rounded-r text-left"
-            v-for="header in props.headers"
-            :key="header"
-          >
+          <th scope="col" class="p-3 first:rounded-l last:rounded-r text-left" v-for="header in props.headers"
+            :key="header">
             {{ header }}
           </th>
         </tr>
@@ -16,10 +12,14 @@
       <tbody>
         <tr
           class="border-b last:border-0 border-neutral-300 dark:border-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800"
-          v-for="(data, index) in props.datalist"
-          :key="index"
-        >
-          <td scope="row" class="p-3" v-for="item in data" :key="item">{{ item }}</td>
+          v-for="(data, dataIndex) in props.datalist" :key="dataIndex">
+          <td scope="row" class="p-3" v-for="(item, itemIndex) in data" :key="itemIndex">
+            <div class="truncate"
+              :class="[props.tableConfig[itemIndex].width ? `max-w-[${props.tableConfig[itemIndex].width}]` : '']">{{
+                item
+              }}
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -30,6 +30,11 @@
 export interface IBaseTableProps {
   headers: string[]
   datalist: any[][]
+  tableConfig: IBaseTableColConfig[]
+}
+
+export interface IBaseTableColConfig {
+  width: string
 }
 
 const props = defineProps<IBaseTableProps>()
