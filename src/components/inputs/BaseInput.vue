f4bd9a4e074e6d82c12add69bac5545cd9081693
mreyhanfarabi@ubuntuwsl2 <reyhanfarabione@outlook.com>
2024-05-09 12:33:35 +0700

    fix base input component set default width to full

--- src/components/inputs/BaseInput.vue
+++ src/components/inputs/BaseInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-2">
+  <div class="flex flex-col gap-2 w-full">
     <input
       class="px-2 py-1 rounded text-neutral-900 dark:text-neutral-100 bg-transparent border border-neutral-800/20 dark:border-neutral-200/20"
       v-bind="$attrs"
